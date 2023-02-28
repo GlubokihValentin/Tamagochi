b1f2717fd88bd9e9cc488275ee654fb3cc6ebf95
Valentin <val-glubokih@yandex.ru>
2023-02-28 21:45:09 +0300

    Добавил метод вывода данных

--- tamagochi.py
+++ tamagochi.py
@@ -1,7 +1,12 @@
+type_of_animal = input("Введите вид персонажа: ")
+name = input("Введите имя персонажа: ")
+sex = input("Введите пол персонажа: ")
+color = input("Введите цвет персонажа: ")
+
 class Animal:
-    def __init__(self, name, type, sex, color):
+    def __init__(self, name, type_of_animal, sex, color):
         self.name = name
-        self.type = type
+        self.type_of_animal = type_of_animal
         self.age = 1
         self.sex = sex
         self.color = color
@@ -13,9 +18,15 @@ class Animal:
         self.cleanliness = 0
         self.upbringing = 0
 
-def character_selection():
-    type = input("Введите вид персонажа: ")
-    name = input("Введите имя персонажа: ");
+    def character_selection(self):
+
+        print(f"Type: {self.type_of_animal}\n  Name: {self.name}\n Age: {self.age}\n Sex: {self.sex}\n Color: {self.color}\n "
+              f"Health: {self.health}\n Hunger: {self.hunger}\n Craving: {self.craving}\n Mood: {self.mood}\n "
+              f"Vivacity: {self.vivacity}\n Cleanliness: {self.cleanliness}\n Upbringing: {self.upbringing}")
+
+
+Cat = Animal(name, type_of_animal, sex, color)
+Cat.character_selection()
 
 
 
@@ -34,14 +45,3 @@ def character_selection():
 
 
 
-
-
-
-
-
-
-cat = Animal()
-dog = Animal()
-rat = Animal()
-hamster = Animal()
-parrot = Animal()
