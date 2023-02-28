cdcdc3db391f17762868dc534693563f4b826b5d
ivashal <shalaevia@gmail.com>
2023-02-28 20:50:40 +0300

    add achiv file

---
+++ achiv.py
@@ -0,0 +1,21 @@
+def achiv():
+    pass
+
+
+def birth(): # Первая ачивка, выдается при рождении
+    pass
+
+def healt100(): # Первое достижение 100% уровня жизни
+    pass
+
+def age
+
+def hunger100(): # сытость 100%
+    pass
+
+def crawing100(): #
+    pass
+
+
+def jovial(): # Весельчак
+    pass
