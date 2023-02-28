63e8ff7f7ce4bc4879d8fef89a5fdf04aab26f06
Vermilion665 <Vermilion666@list.ru>
2023-02-28 20:50:52 +0300

    add cleaning

---
+++ cleaning.py
@@ -0,0 +1,20 @@
+#
+# class Animal:
+#     def __init__(self, name, type, sex, color):
+#         self.name = name
+#         self.type = type
+#         self.age = 1
+#         self.sex = sex
+#         self.color = color
+#         self.health = 80
+#         self.hunger = 80
+#         self.craving = 80
+#         self.mood = 50
+#         self.vivacity = 10
+#         self.cleanliness = 0
+#         self.upbringing = 0
+
+
+def cleaning():
+    if cleanliness == 0:
+        print('Ваш питомиц нагадил кучу выше Эйфилевой башни, нажмите "U" чтоб убрать за пиздюком')
