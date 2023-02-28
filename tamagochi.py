2dc71cfd61852a2a8ac870a435c62538f8164716
Valentin <val-glubokih@yandex.ru>
2023-02-28 20:01:55 +0300

    add class Animal

--- tamagochi.py
+++ tamagochi.py
@@ -1,8 +1,41 @@
 class Animal:
-    pass
+    def __init__(self, name, type, sex, color):
+        self.name = name
+        self.type = type
+        self.age = 1
+        self.sex = sex
+        self.color = color
+        self.health = 80
+        self.hunger = 80
+        self.craving = 80
+        self.mood = 50
+        self.vivacity = 10
+        self.cleanliness = 0
+        self.upbringing = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 cat = Animal()
 dog = Animal()
 rat = Animal()
 hamster = Animal()
+parrot = Animal()
