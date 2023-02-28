2611d86d0847b0e7a8742780b6dd019cfeb2c3be
Test Student <vova.derkach.89@mail.ru>
2023-02-28 21:55:40 +0300

    first commit

---
+++ sleep_metod.py
@@ -0,0 +1,24 @@
+import datetime
+
+# vivacity = 10
+# health = 80
+
+current_date_time = datetime.datetime.now()
+current_time = current_date_time.time()
+print(current_time)
+
+# current_date_time = datetime.datetime.now()
+# current_time = current_date_time.time()
+# print(current_time)
+#
+#
+# current_date_time = datetime.time.now()
+# # print(current_date_time)
+#
+# def sleep():
+#     if (vivacity <= 50 and health <= 30) or (current_date_time = datetime.time(20,59,59)):
+#         print('Tamagotchi go to sleep')
+#     else:
+#         print('Tamagotchi does not sleep')
+#
+# sleep()
