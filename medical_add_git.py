ccfc8a3a9095e1e7a2c3f6add5c6ad803b190d81
NovozhilovAV <tema.reida87@mail.ru>
2023-03-02 21:21:35 +0300

    refresh medical

--- medical_add_git.py
+++ medical_add_git.py
@@ -1,13 +1,30 @@
 # Ветка medical
 # прописываем все свойства по лечению персонажа - взаимосвязано со здоровьем  и настроением
 
-# self.health = 80     # здоровье  изначально
-# self.vivacity = 10     #  бодрость изначально
+# import tamagochi.py
 
-def medical():
-    if (health <= 35 and health > 15) and (vivacity <= 20 and vivacity > 15):
+# health = 33     # здоровье  изначально
+# vivacity = 10     #  бодрость изначально
+
+def medical(health, vivacity):
+    if (35 >= health > 15) and (20 >= vivacity > 15):
         print('Ваше здоровье ухудшилось,необходимо подлечиться!!')
-    if (health <= 15 and health > 5) and (vivacity <= 15 and vivacity > 5):
+    #     тут необходимо просать условие чтобы здоровье и бодрость изменялись
+    # @health.setter
+    # def health(self,a):
+    # self._health = 100
+
+    # @vivacity.setter
+    # def vivacity(self, f):
+    #     self._vivacity = 100
+
+    elif (15 >= health > 5) and (15 >= vivacity > 5):
         print('Вам необходимы стимуляторы!!')
-    if (health <= 5 and health > 1) and (vivacity <= 5 and vivacity > 1):
-            print('Срочно реанимацию!!')
+        #     тут необходимо просать условие чтобы здоровье и бодрость изменялись
+    elif (5 >= health > 1) and (5 >= vivacity > 1):
+        print('Срочно реанимацию!!')
+        #     тут необходимо просать условие чтобы здоровье и бодрость изменялись
+    elif (45 >= health > 35) and (45 >= vivacity > 35):
+        print('Энегретик тебе в помощь,немного приуныл!! или SNIKERS-ni')
+
+# medical()
