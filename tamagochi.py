a9fc9abd1ee0b0f30d6082f8449308d6450327bc
Valentin <val-glubokih@yandex.ru>
2023-03-02 20:14:02 +0300

    Добавил геттеры и сеттеры

--- tamagochi.py
+++ tamagochi.py
@@ -11,15 +11,80 @@ class Animal:
         self.age = 1
         self.is_male = is_male
         self.color = color
-        self.health = 80
-        self.hunger = 80
-        self.craving = 80
-        self.mood = 50
-        self.vivacity = 10
-        self.cleanliness = 0
-        self.upbringing = 0
-        self.is_sleep = False
+        self._health = 80
+        self._hunger = 80
+        self._craving = 80
+        self._mood = 50
+        self._vivacity = 10
+        self._cleanliness = 0
+        self._upbringing = 0
+        self._is_sleep = False
 
+    @property
+    def health(self):
+        return self._health
+
+    @health.setter
+    def health(self, a):
+        self._health = a
+
+    @property
+    def hunger(self):
+        return self._hunger
+
+    @hunger.setter
+    def hunger(self, b):
+        self._hunger = b
+
+    @property
+    def craving(self):
+        return self._craving
+
+    @craving.setter
+    def craving(self, c):
+        self._craving = c
+
+    @property
+    def mood(self):
+        return self._mood
+
+    @mood.setter
+    def mood(self, e):
+        self._mood = e
+
+    @property
+    def vivacity(self):
+        return self._vivacity
+
+    @vivacity.setter
+    def vivacity(self, f):
+        self._vivacity = f
+
+    @property
+    def cleanliness(self):
+        return self._cleanliness
+
+    @cleanliness.setter
+    def cleanliness(self, g):
+        self._cleanliness = g
+
+    @property
+    def upbringing(self):
+        return self._upbringing
+
+    @upbringing.setter
+    def upbringing(self, h):
+        self._upbringing = h
+
+    @property
+    def is_sleep(self):
+        return self._is_sleep
+
+    @is_sleep.setter
+    def is_sleep(self, h):
+        self._is_sleep = h
+
+    
     def character_selection(self):
 
         print(f"Type: {self.type_of_animal}\n  Name: {self.name}\n Age: {self.age}\n Is_male: {self.is_male}\n Color: {self.color}\n "
